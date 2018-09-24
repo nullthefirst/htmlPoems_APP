5468962331a09c06c84680c7614beef2365eaa0e
Usheninte <jrandangana@gmail.com>
2018-09-24 19:22:06 +0100

    initial React commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
