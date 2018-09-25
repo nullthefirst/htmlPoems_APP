b446febaf1cd589400212fddaea9ebffdcff56f4
Usheninte <jrandangana@gmail.com>
2018-09-25 07:45:05 +0100

    added Bulma

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import CardExampleCard from './components/card';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <CardExampleCard />
       </div>
     );
   }
