e9f71137ffae3727ddd79a2e2c8999c4aaddd3ad
Usheninte <jrandangana@gmail.com>
2018-10-28 07:39:09 +0100

    editing music

--- src/components/views/music.js
+++ src/components/views/music.js
@@ -11,24 +11,26 @@ export default class Music extends Component {
       <div>
         <div class="container">
           <div class="row">
-            <div class="col-12 col-sm-4"></div>
+            <div class="col-sm-4"></div>
             <div class="col-12 col-sm-4">
               <Card>
                 <iframe title="STAT1C B1NARY" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/498935088&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
               </Card>
             </div>
-            <div class="col-12 col-sm-4"></div>
+            <div class="col-sm-4"></div>
           </div>
         </div>
-        <Grid>
-          <Col md={4}>
-            <Well>
-              <iframe title="NackUs" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/480556836&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-            </Well>
-          </Col>
-          <Col xsHidden md={4}></Col>
-          <Col xsHidden md={4}></Col>  
-        </Grid>
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-sm-4">
+              <Card>
+                <iframe title="NackUs" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/480556836&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+              </Card>
+            </div>
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4"></div>
+          </div>
+        </div>
       </div>
     );
   }
