e2a89d0a881563c17af628048fe247e559d36b05
Usheninte <jrandangana@gmail.com>
2018-10-28 07:20:45 +0100

    editing content

--- src/components/views/content.js
+++ src/components/views/content.js
@@ -20,26 +20,11 @@ export default class Content extends Component {
             <div class="col-sm-4"></div>
           </div>
         </div>
-        <Grid>
-          <Col md={4}>
-            <Well>
-              <Image src="https://res.cloudinary.com/poetrique/image/upload/v1537473496/htmlpoems/mambo/z02.jpg" alt="htmlPoems" title="htmlPoems Content" responsive />
-              &copy; Mambo
-            </Well>          
-          </Col>
-          <Col xsHidden md={4}></Col>
-          <Col md={4}>
-            <Well>
-              <Image src="http://res.cloudinary.com/poetrique/image/upload/v1536065967/htmlpoems/wonexo/wonexo_4.png" alt="htmlPoems" title="htmlPoems Content" responsive />
-              &copy; Wonexo
-            </Well>
-          </Col>
-        </Grid>
         <div class="container">
           <div class="row">
             <div class="col-sm-4">
               <Card>
-                <CardImage className="img-fluid" src="" alt="htmlPoems" title="htmlPoems Content" />
+                <CardImage className="img-fluid" src="https://res.cloudinary.com/poetrique/image/upload/v1537473496/htmlpoems/mambo/z02.jpg" alt="htmlPoems" title="htmlPoems Content" />
                 <CardBody>
                   <CardTitle>Card title</CardTitle>
                 </CardBody>
@@ -48,7 +33,7 @@ export default class Content extends Component {
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
               <Card>
-                <CardImage className="img-fluid" src="" alt="htmlPoems" title="htmlPoems Content" />
+                <CardImage className="img-fluid" src="http://res.cloudinary.com/poetrique/image/upload/v1536065967/htmlpoems/wonexo/wonexo_4.png" alt="htmlPoems" title="htmlPoems Content" />
                 <CardBody>
                   <CardTitle>Card title</CardTitle>
                 </CardBody>
