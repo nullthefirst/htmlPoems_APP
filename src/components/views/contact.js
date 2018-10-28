3a3cd670b3aa0ea252c0c6ec95d8b10bf1d4864c
Usheninte <jrandangana@gmail.com>
2018-10-28 07:59:04 +0100

    edited contact

---
+++ src/components/views/contact.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Card, CardBody, CardImage, CardTitle } from 'mdbreact';
+
+import '../Artis.css';
+
+export default class Contact extends Component {
+  render() {
+    return(
+      <div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-12 col-sm-4">
+              <Card>
+                <p><b>htmlPoems.com</b> is a open and inclusive online creative space. If you have created something beautiful, and would like to share it with the world, simply send us a <b>DM</b> on Instagram. We will place your art in our queue, and publish it to the world, in a wonderful and creative way.</p>
+              </Card>
+            </div>
+            <div class="col-sm-4"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
