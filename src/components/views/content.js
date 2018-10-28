f2825fd19673b909482516e74b875961ea406fce
Usheninte <jrandangana@gmail.com>
2018-10-28 07:23:02 +0100

    editing content

--- src/components/views/content.js
+++ src/components/views/content.js
@@ -26,7 +26,7 @@ export default class Content extends Component {
               <Card>
                 <CardImage className="img-fluid" src="https://res.cloudinary.com/poetrique/image/upload/v1537473496/htmlpoems/mambo/z02.jpg" alt="htmlPoems" title="htmlPoems Content" />
                 <CardBody>
-                  <CardTitle>Card title</CardTitle>
+                  <CardTitle>&copy; Mambo</CardTitle>
                 </CardBody>
               </Card>
             </div>
@@ -35,46 +35,31 @@ export default class Content extends Component {
               <Card>
                 <CardImage className="img-fluid" src="http://res.cloudinary.com/poetrique/image/upload/v1536065967/htmlpoems/wonexo/wonexo_4.png" alt="htmlPoems" title="htmlPoems Content" />
                 <CardBody>
-                  <CardTitle>Card title</CardTitle>
+                  <CardTitle>&copy; Wonexo</CardTitle>
                 </CardBody>
               </Card>
             </div>
           </div>
         </div>
-        <Grid>
-          <Col md={4}>
-            <Well>
-              <Image src="http://res.cloudinary.com/poetrique/image/upload/v1536065174/htmlpoems/paul_joe/pj_3.jpg" alt="htmlPoems" title="htmlPoems Content" responsive />
-              &copy; PeeJay
-            </Well>
-          </Col>
-          <Col md={4}>
-            <Well>
-              <Image src="https://res.cloudinary.com/poetrique/image/upload/v1537473495/htmlpoems/mambo/z01.jpg" alt="htmlPoems" title="htmlPoems Content" responsive />
-              &copy; Mambo
-            </Well>
-          </Col>
-          <Col xsHidden md={4}></Col>
-        </Grid>
         <div class="container">
           <div class="row">
             <div class="col-sm-4">
               <Card>
-                <CardImage className="img-fluid" src="" alt="htmlPoems" title="htmlPoems Content" />
+                <CardImage className="img-fluid" src="http://res.cloudinary.com/poetrique/image/upload/v1536065174/htmlpoems/paul_joe/pj_3.jpg" alt="htmlPoems" title="htmlPoems Content" />
                 <CardBody>
-                  <CardTitle>Card title</CardTitle>
+                  <CardTitle>&copy; PeeJay</CardTitle>
+                </CardBody>
+              </Card>
+            </div>
+            <div class="col-sm-4">
+              <Card>
+                <CardImage className="img-fluid" src="https://res.cloudinary.com/poetrique/image/upload/v1537473495/htmlpoems/mambo/z01.jpg" alt="htmlPoems" title="htmlPoems Content" />
+                <CardBody>
+                  <CardTitle>&copy; Mambo</CardTitle>
                 </CardBody>
               </Card>
             </div>
             <div class="col-sm-4"></div>
-            <div class="col-sm-4">
-              <Card>
-                <CardImage className="img-fluid" src="" alt="htmlPoems" title="htmlPoems Content" />
-                <CardBody>
-                  <CardTitle>Card title</CardTitle>
-                </CardBody>
-              </Card>
-            </div>
           </div>
         </div>
         <Grid>
