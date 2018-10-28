0057cbdeba835f9228fd7112d79e325e2e6308ab
Usheninte <jrandangana@gmail.com>
2018-10-28 07:34:46 +0100

    editing music

--- src/components/views/music.js
+++ src/components/views/music.js
@@ -9,19 +9,14 @@ export default class Music extends Component {
   render() {
     return(
       <div>
-        <Grid>
-          <Col xsHidden md={4}></Col>
-          <Col md={4}>
-            <Well>
-              <iframe title="STAT1C B1NARY" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/498935088&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-            </Well>
-          </Col>
-          <Col xsHidden md={4}></Col>          
-        </Grid>
         <div class="container">
           <div class="row">
             <div class="col-12 col-sm-4"></div>
-            <div class="col-12 col-sm-4"></div>
+            <div class="col-12 col-sm-4">
+              <Card>
+                <iframe title="STAT1C B1NARY" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/498935088&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+              </Card>
+            </div>
             <div class="col-12 col-sm-4"></div>
           </div>
         </div>
